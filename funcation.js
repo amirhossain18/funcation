2b14f1f75b4f602e313337a910dbb6a8d2934e03
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-09-14 20:31:29 +0600

    namta

--- funcation.js
+++ funcation.js
@@ -9,13 +9,44 @@ function bringpoteto(price){
     console.log("nen poteto chips")
     console.log(price)
 }
-var taka1=200;
-var taka2=300;
-var taka3=500;
+// var taka1=200;
+// var taka2=300;
+// var taka3=500;
 
-function Totaltaka(price1 , price2 ) {
-    var total=taka1+taka2+taka3;
-    console.log(total);
+function totaltaka(price1 , price2, price3 ) {
+    var total=price1+price2+price3;
+    // console.log(total);
+    return total;
 }
 
-Totaltaka(taka1, taka2, taka3);
+var money = totaltaka(10,10,10);
+
+function totalmoney(person){
+    var collections =600;
+    console.log(collections);
+    var totalcollection= collections * person
+    return totalcollection;
+}
+
+var totalOurtk = totalmoney(30);
+console.log(totalOurtk);
+
+// function multiplication(n){
+//     for(var i=1; i<=10; i++){
+//         console.log(`${n} * ${i}=${n*i}`);
+//     }
+
+// }
+
+// var namta= multiplication(13);
+// console.log(namta);
+
+function multiplication(n){
+    for(var i=1; i<=10; i++){
+   
+     var anser= ( `${i} x ${n}=${i*n}  ` )
+    }
+return anser;
+}
+
+var amount= multiplication(13);
